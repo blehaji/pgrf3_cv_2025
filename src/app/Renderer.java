850ca2d4adde77322f4f25c663bbdf78381607f0
Jiří Bleha <jiri.bleha@uhk.cz>
2024-11-24 18:20:06 +0100

    Implement light and information visualisation

--- src/app/Renderer.java
+++ src/app/Renderer.java
@@ -1,6 +1,7 @@
 package app;
 
 import app.solid.Grid;
+import lwjglutils.OGLTextRenderer;
 import lwjglutils.OGLTexture;
 import lwjglutils.OGLTexture2D;
 import org.lwjgl.BufferUtils;
@@ -21,18 +22,38 @@ public class Renderer extends AbstractRenderer {
 
     private final static String TEXTURE_PATH = "textures/";
 
+    private enum PolygonMode {
+        LINE(GL_LINE),
+        POINT(GL_POINT),
+        FILL(GL_FILL);
+
+        private final int value;
+        PolygonMode(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
     private Camera camera;
     private Mat4 projectionMatrix;
     private final Map<String, OGLTexture> textures = new HashMap<>();
+    private final List<String> textureNames = new ArrayList<>();
+    private int textureIndex;
     private final List<Grid> grids = new ArrayList<>();
-    private Grid grid;
-    private int polygonMode = GL_FILL;
+    private Grid grid, light;
+    private PolygonMode polygonMode = PolygonMode.FILL;
     private boolean isPerspectiveProjection = true;
     private boolean isMousePressed = false;
     private final double[] mouseOrigin = new double[2];
+    private Vec3D lightPosition = new Vec3D(1.5, 0, 1.5);
 
     @Override
     public void init() {
+        textRenderer = new OGLTextRenderer(width, height);
+
         loadTextures();
 
         Grid floor = new Grid();
@@ -40,20 +61,24 @@ public class Renderer extends AbstractRenderer {
         grid = new Grid(100, 100, GL_TRIANGLES, Grid.FuncType.SPHERE);
         grid.setColor(1, 1, 0);
         grid.translate(new Vec3D(0, 0, 1));
-        grid.setTexture(textures.get("globe.png"));
+        setTexture(grid);
         grid.setColorMode(Grid.ColorMode.TEXTURE);
 
         grids.add(floor);
         grids.add(grid);
 
+        light = new Grid(50, 50, GL_TRIANGLES, Grid.FuncType.SPHERE);
+        light.setColor(1, 1, 1);
+        light.setEnableLighting(false);
+        light.scale(new Vec3D(0.1));
+        light.translate(lightPosition);
+
         camera = new Camera()
-                .withPosition(new Vec3D(0, -2, 1))
+                .withPosition(new Vec3D(0, -3, 1))
                 .withAzimuth(Math.toRadians(90))
                 .withZenith(Math.toRadians(-15))
                 .withFirstPerson(true);
         updateProjectionMatrix();
-
-        updateGrids();
 
         glEnable(GL_DEPTH_TEST);
         glEnable(GL_PRIMITIVE_RESTART);
@@ -73,10 +98,21 @@ public class Renderer extends AbstractRenderer {
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
+            textureNames.add(file.getName());
         }
+        textureIndex = textureNames.size() - 1;
+    }
+
+    private void setTexture(Grid grid) {
+        grid.setTexture(textures.get(textureNames.get(textureIndex)));
     }
 
     private void updateGrids() {
+        if (light != null) {
+            light.setProjectionMatrix(projectionMatrix);
+            light.setViewMatrix(camera.getViewMatrix());
+        }
+
         for (Grid grid : grids) {
             grid.setProjectionMatrix(projectionMatrix);
             grid.setViewMatrix(camera.getViewMatrix());
@@ -87,19 +123,34 @@ public class Renderer extends AbstractRenderer {
     public void display() {
         glViewport(0, 0, width, height);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glPolygonMode(GL_FRONT_AND_BACK, polygonMode);
+        glPolygonMode(GL_FRONT_AND_BACK, polygonMode.getValue());
+
+        light.draw();
+        float[] lightPositionF = new float[] {
+                (float) lightPosition.getX(),
+                (float) lightPosition.getY(),
+                (float) lightPosition.getZ()
+        };
 
         for (Grid grid : grids) {
+            grid.setLightPosition(lightPositionF);
             grid.draw();
         }
+
+        drawText();
+    }
+
+    private void drawText() {
+        textRenderer.addStr2D(5, 25, String.format("[TAB] Projection type: %s", isPerspectiveProjection ? "perspective" : "orthogonal"));
+        textRenderer.addStr2D(5, 45, String.format("[F] Function type: %s", grid.getFuncType()));
+        textRenderer.addStr2D(5, 65, String.format("[C] Color mode: %s", grid.getColorMode()));
+        textRenderer.addStr2D(5, 85, String.format("[P] Polygon mode: %s", polygonMode));
+        textRenderer.addStr2D(5, 105, String.format("[T] Texture: %s", textureNames.get(textureIndex)));
     }
 
     private void changePolygonMode() {
-        switch (polygonMode) {
-            case GL_FILL -> polygonMode = GL_LINE;
-            case GL_LINE -> polygonMode = GL_POINT;
-            case GL_POINT -> polygonMode = GL_FILL;
-        }
+        PolygonMode[] polygonModes = PolygonMode.values();
+        polygonMode = polygonModes[(polygonMode.ordinal() + 1) % polygonModes.length];
     }
 
     private void updateProjectionMatrix() {
@@ -137,11 +188,9 @@ public class Renderer extends AbstractRenderer {
             case GLFW.GLFW_KEY_F:
                 changeFunctionType();
                 break;
-            case GLFW.GLFW_KEY_UP:
-                grid.translate(new Vec3D(0, 0, 0.5));
-                break;
-            case GLFW.GLFW_KEY_DOWN:
-                grid.translate(new Vec3D(0, 0, -0.5));
+            case GLFW.GLFW_KEY_T:
+                textureIndex = (textureIndex + 1) % textures.size();
+                setTexture(grid);
                 break;
         }
     }
@@ -157,6 +206,20 @@ public class Renderer extends AbstractRenderer {
             case GLFW.GLFW_KEY_X -> camera = camera.down(speed);
         }
         updateGrids();
+    }
+
+    private void moveLight(int key) {
+        double speed = 0.02;
+        final Vec3D direction;
+        switch (key) {
+            case GLFW.GLFW_KEY_RIGHT -> direction = new Vec3D(speed, 0, 0);
+            case GLFW.GLFW_KEY_LEFT -> direction = new Vec3D(-speed, 0, 0);
+            case GLFW.GLFW_KEY_UP -> direction = new Vec3D(0, speed, 0);
+            case GLFW.GLFW_KEY_DOWN -> direction = new Vec3D(0, -speed, 0);
+            default -> direction  = new Vec3D(0, 0, 0);
+        }
+        light.translate(direction);
+        lightPosition = lightPosition.add(direction);
     }
 
     private void rotateCamera(double x, double y) {
@@ -176,6 +239,7 @@ public class Renderer extends AbstractRenderer {
 
             if (action == GLFW.GLFW_PRESS || action == GLFW.GLFW_REPEAT) {
                 moveCamera(key);
+                moveLight(key);
             }
         }
     };
@@ -231,6 +295,9 @@ public class Renderer extends AbstractRenderer {
             width = w;
             height = h;
             updateProjectionMatrix();
+            if (textRenderer != null) {
+                textRenderer.resize(width, height);
+            }
         }
     };
 
